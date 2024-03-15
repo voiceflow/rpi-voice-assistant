39bbf0ab345a754050dcd448e54dfa3117d70b07
Theresa <theresa.hradilakgmail.com>
2024-03-15 16:26:55 +0000

    Add LEDStatusManager class to start implementing LED status monitor.

--- dialogbank/main.py
+++ dialogbank/main.py
@@ -15,6 +15,106 @@ JSON = Dict[str, Any]
 
 import tty, sys, termios
 
+
+from blinkt import set_pixel, show
+import socket
+
+class LEDStatusManager():
+
+    NO_DATA = 0
+    WIFI_OFF = 1
+    WIFI_ON = 2
+    BOOTING = 1
+    WAITING = 2
+    CONVERSATION_RUNNING = 3
+    UNSUCCESSFUL_REQUEST = 1
+    SUCCESSFUL_REQUEST = 2
+    LISTENING = 1
+
+    colors = {
+        'RED' : (255, 0, 0),
+        'GREEN' : (0, 255, 0),
+        'BLUE' : (0, 0, 255),
+        'YELLOW': (255, 255, 0),
+        'WHITE': (0, 0, 0)
+    }
+
+    #cheap bimap to allow easy access
+    leds = {
+        0 : 'WIFI',
+        1 : 'APPLICATION',
+        2 : 'GOOGLE_ASR_API',
+        3 : 'VOICEFLOW_API',
+        4 : 'ELEVENLABS_API',
+        5 : 'LISTENING',
+        'WIFI' : 0,
+        'APPLICATION' : 1,
+        'GOOGLE_ASR_API' : 2,
+        'VOICEFLOW_API' : 3,
+        'ELEVENLABS_API' : 4,
+        'LISTENING' : 5
+    }
+
+    config = {
+        #LED #Status #Color #Interpretation
+        0 : {NO_DATA: 'WHITE',
+             WIFI_OFF: 'RED', 
+             WIFI_ON: 'GREEN'},
+        1 : {NO_DATA: 'WHITE',
+             BOOTING: 'YELLOW',
+             WAITING: 'BLUE',
+             CONVERSATION_RUNNING: 'GREEN'},
+        2 : {NO_DATA: 'WHITE',
+             UNSUCCESSFUL_REQUEST: 'RED',
+             SUCCESSFUL_REQUEST: 'GREEN'},
+        3 : {NO_DATA: 'WHITE',
+             UNSUCCESSFUL_REQUEST: 'RED',
+             SUCCESSFUL_REQUEST: 'GREEN'},
+        4 : {NO_DATA: 'WHITE',
+             UNSUCCESSFUL_REQUEST: 'RED',
+             SUCCESSFUL_REQUEST: 'GREEN'},
+        5 : {NO_DATA: 'WHITE',
+             LISTENING: 'BLUE'},
+    }
+
+    status = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    def show(self):
+        for led, value in self.config.items():
+            cur_status = value[self.status[led]]
+            print(cur_status)
+            print(*self.colors[cur_status])
+            set_pixel(led, *self.colors[cur_status])
+            show()
+
+    def update(self, context, status):
+        led = self.leds[context]
+        self.status[led] = status
+        print(self.status)
+        self.show()
+
+    def update_wifi_availability(self):
+        if self.check_internet_availability():
+            self.update('WIFI', self.WIFI_ON)
+        else:
+            self.update('WIFI', self.WIFI_OFF)
+        
+    def check_internet_availability(self, host="8.8.8.8", port=53, timeout=3):
+        """
+        Host: 8.8.8.8 (google-public-dns-a.google.com)
+        OpenPort: 53/tcp
+        Service: domain (DNS/TCP)
+        """
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+            return True
+        except socket.error as ex:
+            print(ex)
+            return False
+
+
+
 #TODO: somewhat hacky keyboard input but works without sudo, check if better options.
 filedescriptors = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin)
@@ -62,6 +162,8 @@ def run_dialogbench(voiceflow_client: Voiceflow, google_asr_client: speech.Speec
         vf_response = voiceflow_client.interact.launch()
         end = handle_vf_response(voiceflow_client, vf_response, elevenlabs_client, audio_player)
 
+        set_pixel(7,255,0,0)
+        show()
         while not end:
             audio_player.beep() #signal start of listening to user
             log.debug("[Voice Assistant]: Start listening.")
@@ -85,6 +187,9 @@ def run_dialogbench(voiceflow_client: Voiceflow, google_asr_client: speech.Speec
             end = handle_vf_response(voiceflow_client, vf_response, elevenlabs_client, audio_player)
 
 def main():
+
+    led_status_manager = LEDStatusManager()
+    led_status_manager.update_wifi_availability()
 
     voiceflow_client = Voiceflow(
         api_key=os.getenv('VF_API_KEY', "dummy_key"),
@@ -112,14 +217,16 @@ def main():
     while True:
         voiceflow_client.user_id = uuid.uuid4()
         log.debug("[Voice Assistant]: Starting voice assistant", voiceflow_user_id=voiceflow_client.user_id)
+        led_status_manager.update_wifi_availability()
         input("Press Enter to start the voice assistant...")
         p = Process(target=run_dialogbench, args=(voiceflow_client, google_asr_client, google_streaming_config, elevenlabs_client, audio_player))
         p.start()
         while True:  # making a loop
-            x=sys.stdin.read(1)[0]
+            x = sys.stdin.read(1)[0]
+            led_status_manager.update_wifi_availability()
             if (x == 'q'):
                 p.terminate()
-                print("Terminated process")
+                log.debug("Terminated process due to keyboard input.")
                 break
 
 #TODO on exit kill all processes!
